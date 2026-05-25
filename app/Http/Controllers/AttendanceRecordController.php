6675cefeb0241cf545604bf5b3fec84529c6d6af
TANGHuyming <2023277tang@aupp.edu.kh>
2026-05-25 16:16:48 +0700

    Add attendance controller and routes

---
+++ app/Http/Controllers/AttendanceRecordController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\AttendanceRecord as AttendanceRecord;
+
+class AttendanceRecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return AttendanceRecord::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'employee_name' => 'required|string|max:255',
+            'check_in_time' => 'required|date_format:H:i:s',
+            'check_out_time' => 'date_format:H:i:s',
+            'date' => 'required|date_format:Y-m-d',
+        ]);
+
+        $newRecord = AttendanceRecord::create($validated);
+
+        return response()->json($newRecord, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AttendanceRecord $attendance)
+    {
+        return $attendance;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AttendanceRecord $attendance)
+    {
+        $validated = $request->validate([
+            'employee_name' => 'required|string|max:255',
+            'check_in_time' => 'required|date_format:H:i:s',
+            'check_out_time' => 'date_format:H:i:s',
+            'date' => 'required|date_format:Y-m-d',
+        ]);
+
+        $attendance->update($validated);
+
+        return $attendance;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AttendanceRecord $attendance)
+    {
+        $attendance->delete();
+
+        return response()->noContent();
+    }
+}
--- app/Models/AttendanceRecord.php
+++ app/Models/AttendanceRecord.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AttendanceRecord extends Model
+class AttendenceRecord extends Model
 {
-    //
+    protected $fillable = [
+        "employee_id",
+        "check_in_time",
+        "check_out_time",
+        "date",
+    ];
 }
